2f3ef6af86c6fbcf6f6ef6234309712fe3e4fcc4
vladcristianmarin <vladmarin34@gmail.com>
2021-12-02 17:45:29 +0200

    removed 2 dependencies and added gets for activ

--- src/routers/activity.router.js
+++ src/routers/activity.router.js
@@ -16,4 +16,23 @@ router.post('/activity', auth, async (req, res) => {
 	}
 });
 
+router.get('/activities', auth, async (req, res) => {
+	try {
+		const activities = await req.prof.getActivities();
+		res.status(200).send(activities);
+	} catch (error) {
+		res.status(500).send({ error: error.message });
+	}
+});
+
+router.get('/activity/:code', async (req, res) => {
+	try {
+		console.log(req.params.code);
+		const activity = await Activity.findByCode(req.params.code);
+		res.status(200).send(activity);
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+});
+
 module.exports = router;
