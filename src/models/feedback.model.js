56a5c977aeecc81c99c3f7cde2f91195797972d5
Mihalache Andrei <andreimircea.mihalache@gmail.com>
2021-12-04 20:03:08 +0200

    modified ? into 2

--- src/models/feedback.model.js
+++ src/models/feedback.model.js
@@ -1,47 +1,47 @@
-const { sequelize } = require('../db/sequelize');
-const { DataTypes } = require('sequelize');
+const { sequelize } = require("../db/sequelize");
+const { DataTypes } = require("sequelize");
 
 const Feedback = sequelize.define(
-	'Feedback',
-	{
-		id: {
-			type: DataTypes.UUID,
-			allowNull: false,
-			defaultValue: DataTypes.UUIDV4,
-			primaryKey: true,
-			set(_value) {
-				return this.setDataValue('id', DataTypes.UUIDV4);
-			},
-		},
-		feedback: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			set(value) {
-				if (typeof value === 'string' || value instanceof String)
-					this.setDataValue('feedback', value.trim());
-			},
-			validate: {
-				isValidFeedback(value) {
-					if (!['1', '-1', '0', '?'].includes(value))
-						throw new Error(
-							'Invalid feedback value! Available values: 1: good, -1: bad, 0: wow, ?: unclear'
-						);
-				},
-			},
-		},
-		activityId: {
-			type: DataTypes.UUID,
-			allowNull: false,
-		},
-	},
-	{
-		timestamps: true,
-	}
+  "Feedback",
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      set(_value) {
+        return this.setDataValue("id", DataTypes.UUIDV4);
+      },
+    },
+    feedback: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        if (typeof value === "string" || value instanceof String)
+          this.setDataValue("feedback", value.trim());
+      },
+      validate: {
+        isValidFeedback(value) {
+          if (!["1", "-1", "0", "2"].includes(value))
+            throw new Error(
+              "Invalid feedback value! Available values: 1: good, -1: bad, 0: wow, 2: unclear"
+            );
+        },
+      },
+    },
+    activityId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 Feedback.associate = () => {
-	const { Activity } = sequelize.models;
-	Feedback.belongsTo(Activity, { foreignKey: 'activityId' });
+  const { Activity } = sequelize.models;
+  Feedback.belongsTo(Activity, { foreignKey: "activityId" });
 };
 
 module.exports = Feedback;
--- src/routers/activity.router.js
+++ src/routers/activity.router.js
@@ -1,97 +1,97 @@
-const express = require('express');
-const auth = require('../middleware/auth');
-const Activity = require('../models/activity.model');
-const Feedback = require('../models/feedback.model');
+const express = require("express");
+const auth = require("../middleware/auth");
+const Activity = require("../models/activity.model");
+const Feedback = require("../models/feedback.model");
 
 const router = new express.Router();
 
-router.post('/activity', auth, async (req, res) => {
-	try {
-		const activity = await Activity.create({
-			profId: req.prof.id,
-			...req.body,
-		});
-		res.status(201).send(activity);
-	} catch (error) {
-		res.status(400).send({ error: error.message });
-	}
+router.post("/activity", auth, async (req, res) => {
+  try {
+    const activity = await Activity.create({
+      profId: req.prof.id,
+      ...req.body,
+    });
+    res.status(201).send(activity);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.get('/activities', auth, async (req, res) => {
-	try {
-		const activities = await req.prof.getActivities();
-		res.status(200).send(activities);
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
+router.get("/activities", auth, async (req, res) => {
+  try {
+    const activities = await req.prof.getActivities();
+    res.status(200).send(activities);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
-router.get('/activity/:code', async (req, res) => {
-	try {
-		const activity = await Activity.findByCode(req.params.code);
-		res.status(200).send(activity);
-	} catch (error) {
-		res.status(400).send({ error: error.message });
-	}
+router.get("/activity/:code", async (req, res) => {
+  try {
+    const activity = await Activity.findByCode(req.params.code);
+    res.status(200).send(activity);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.patch('/activity/:code', auth, async (req, res) => {
-	const updates = Object.keys(req.body);
-	const allowedUpdates = ['name', 'description'];
-	const isValidUpdate = updates.every(
-		(update) =>
-			allowedUpdates.includes(update) &&
-			(typeof req.body[update] === 'string' ||
-				req.body[update] instanceof String)
-	);
+router.patch("/activity/:code", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "description"];
+  const isValidUpdate = updates.every(
+    (update) =>
+      allowedUpdates.includes(update) &&
+      (typeof req.body[update] === "string" ||
+        req.body[update] instanceof String)
+  );
 
-	if (!isValidUpdate) return res.status(400).send({ error: 'Invalid Updates' });
+  if (!isValidUpdate) return res.status(400).send({ error: "Invalid Updates" });
 
-	try {
-		const activity = await Activity.findByCode(req.params.code);
-		updates.forEach((update) => (activity[update] = req.body[update]));
+  try {
+    const activity = await Activity.findByCode(req.params.code);
+    updates.forEach((update) => (activity[update] = req.body[update]));
 
-		await activity.save();
-		res.send(activity);
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
+    await activity.save();
+    res.send(activity);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
-router.delete('/activity/:code', auth, async (req, res) => {
-	try {
-		const activity = await Activity.findByCode(req.params.code);
-		await activity.destroy();
-		res.send(activity);
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
+router.delete("/activity/:code", auth, async (req, res) => {
+  try {
+    const activity = await Activity.findByCode(req.params.code);
+    await activity.destroy();
+    res.send(activity);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
-router.post('/activity/:code/:feedback', async (req, res) => {
-	try {
-		const activity = await Activity.findByCode(req.params.code);
-		const feedback = await activity.addFb(req.params.feedback);
-		res.send({ feedback });
-	} catch (error) {
-		res.status(400).send({ error: error.message });
-	}
+router.post("/activity/:code/:feedback", async (req, res) => {
+  try {
+    const activity = await Activity.findByCode(req.params.code);
+    const feedback = await activity.addFb(req.params.feedback);
+    res.send({ feedback });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.get('/activity/:code/feedbacks', auth, async (req, res) => {
-	try {
-		const activity = await Activity.findByCode(req.params.code);
-		const activities = await req.prof.getActivities();
-		const hasActivity = activities.some((act) => act.id === activity.id);
-		if (!hasActivity) {
-			throw new Error('Not authorized');
-		}
-		const feedbacks = await activity.getFeedbacks();
-		const counter = { 1: 0, '-1': 0, 0: 0, '?': 0 };
-		feedbacks.forEach((item) => counter[item.feedback]++);
-		res.send(counter);
-	} catch (error) {
-		res.status(400).send({ error: error.message });
-	}
+router.get("/activity/:code/feedbacks", auth, async (req, res) => {
+  try {
+    const activity = await Activity.findByCode(req.params.code);
+    const activities = await req.prof.getActivities();
+    const hasActivity = activities.some((act) => act.id === activity.id);
+    if (!hasActivity) {
+      throw new Error("Not authorized");
+    }
+    const feedbacks = await activity.getFeedbacks();
+    const counter = { 1: 0, "-1": 0, 0: 0, 2: 0 };
+    feedbacks.forEach((item) => counter[item.feedback]++);
+    res.send(counter);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 module.exports = router;
